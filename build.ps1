bdc9249429d3232180f8362f607f1207d5aa8163
JimiIT92 <14943331+JimiIT92@users.noreply.github.com>
2026-02-16 01:48:43 +0100

    Updated minecraft version

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 # Variables
 $VERSION = "6.0-beta.1"
-$MC_VERSION = "26.1"
+$MC_VERSION = "26.1-snapshot-7"
 $FULL_PACK_PATH = "Snow on Stairs " + "v" + $VERSION + "-" + $MC_VERSION + ".zip"
 
 # Delete old pack file
