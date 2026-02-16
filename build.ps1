343c2485a9cb81c3b74133f041302a52bfc270e2
JimiIT92 <14943331+JimiIT92@users.noreply.github.com>
2026-02-16 01:26:47 +0100

    Added build script and moved everything under datapack folder

---
+++ build.ps1
@@ -0,0 +1,10 @@
+# Variables
+$VERSION = "6.0-beta.1"
+$MC_VERSION = "26.1"
+$FULL_PACK_PATH = "Snow on Stairs " + "v" + $VERSION + "-" + $MC_VERSION + ".zip"
+
+# Delete old pack file
+Remove-Item $FULL_PACK_PATH -Force
+
+# Create Full pack file
+Compress-Archive -update $("./datapack/*") $FULL_PACK_PATH
